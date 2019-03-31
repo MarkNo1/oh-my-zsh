44b254bcb82bbe2616af826e1ed0c04310e3bd92
markno1 <marcotreglia1@gmail.com>
2019-03-31 12:33:24 +0200

    My first zsh plugin. Fast net utils.

---
+++ plugins/net-alias/net-alias.plugin.zsh
@@ -0,0 +1,43 @@
+: '
+------------------------------------------------------------------------------
+         FILE:  net-alias
+  DESCRIPTION:  oh-my-zsh plugin.
+       AUTHOR:  marco treglia (marco.treglia@akka.eu)
+      VERSION:  1.0.0
+------------------------------------------------------------------------------
+'
+
+function Login() {
+  echo -e "Login into ${1}"
+  xhost +
+  ssh -2 -XY ${1}
+}
+
+SetConnection() {
+  NAME=$(echo "$1" | tr '[a-z]' '[A-Z]')
+  if [[ $NETMASK ]]; then
+    addr="$3@${NETMASK}.$2"
+    alias "$1"='Login ${addr}'
+    export "${NAME}_ADDR"=$addr
+    echo -e "Fast connection to \e[1;28m$1\e[0m : $addr"
+  else
+    echo -e "No env NETMASK is setted"
+  fi
+}
+
+function Copy() {
+  echo -e "Copying from ${1}  to  ${2}"
+  rsync -av --progress -e ssh ${1} ${2}
+}
+
+
+LoadConfiguration(){
+  cfg=".net-alias.cfg"
+  if [ -f  "$HOME/$cfg" ]; then
+    source "${cfg}"
+  else
+    echo "[net-alias]: No $cfg in $HOME."
+  fi
+}
+
+LoadConfiguration
